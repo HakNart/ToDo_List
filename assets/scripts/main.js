9d61a56b8a83d21384002f041878106dc14ae3bb
Kha Tran <khadieptran@gmail.com>
2018-06-05 16:47:32 -0400

    Add function to delete a todo

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,12 @@
 //Toggle the line-through effect when an li is clicked
 $("li").on("click",function(){
-    $(this).toggleClass("clicked");
+    $(this).toggleClass("done");
+})
+
+//Click on X to delete a todo
+$(".delete").on("click",function(event){
+    event.stopPropagation();
+    $(this).parent("li").fadeOut(500,function(){
+        $(this).remove();
+    })
 })
